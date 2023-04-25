5eda63951bb4388aa75025b64a4f664b182c75b5
Jeffrey Nibbs <djcountry100@gmail.com>
2023-04-25 03:10:14 -0700

    add tag cloud start about me

--- src/view/About.jsx
+++ src/view/About.jsx
@@ -1,9 +1,28 @@
+import { useEffect } from 'react'
+import TagCloud from 'TagCloud'
 function About() {
+  useEffect(() => {
+    return () => {
+      const container = '.tagcloud'
+      const texts = ['React', 'Python', 'JavaScript', 'CSS3', 'PHP', 'Tailwindcss', 'Mouse', 'Rolling', 'Sphere', '6KB', 'v2.x']
+      const options = {
+        radius: 300,
+        maxSpeed: 'fast',
+        initSpeed: 'slow',
+        direction: 135,
+        keep: true,
+      }
+
+      TagCloud(container, texts, options)
+    }
+  }, [])
+
   return (
     <section className='flex justify-center items-center my-24'>
-      <div className='max-w-5xl p-12'>
+      <div className='max-w-5xl p-6 bg-blue-400'>
         <h2 className=' text-5xl font-LeagueSpartan text-gray-700'>ABOUT ME</h2>
         <p className=' text-xl font-light font-lato text-gray-400'>About me</p>
+        <span className='tagcloud font-lato font-light text-xl text-gray-50 ml-8'></span>
       </div>
     </section>
   )
