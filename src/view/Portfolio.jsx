57889361908ee49a4ac5e210c5f072ba57093fdf
Jeffrey Nibbs <djcountry100@gmail.com>
2023-05-02 06:08:51 -0700

    start portfolio section

--- src/view/Portfolio.jsx
+++ src/view/Portfolio.jsx
@@ -1,8 +1,33 @@
+import pic1 from '../assets/dn-site.png'
+import pic2 from '../assets/loan-calculator.png'
+import pic3 from '../assets/sous-chef-helper.png'
+import '../view/Portfolio.css'
+
 function Portfolio() {
   return (
-    <section className='text-center p-56'>
-      <h2 className=' font-LeagueSpartan text-5xl text-gray-700'>Portfolio</h2>
-      <p className=' text-xl font-light font-lato text-gray-400'>Port</p>
+    <section className='max-w-5xl h-screen mx-auto bg-gray-100'>
+      <div className='flex text-transparent hover:text-gray-800'>
+        <div className='w-1/2'>
+          <img src={pic1} alt='portfolio picture' />
+        </div>
+        <div className='w-1/2 ml-5 flex flex-col justify-center'>
+          <h2 className='heading text-6xl font-semibold font-LeagueSpartan '>Don Nuva Web Solutions</h2>
+          <p>
+            <span className='text-xl pl-1 text-gray-700 font-light font-lato'>HTML /</span>
+            <span className='text-xl pl-1 text-gray-700 font-light font-lato'>CSS /</span>
+            <span className='text-xl pl-1 text-gray-700 font-light font-lato'>Javascript</span>
+          </p>
+        </div>
+        {/* <img src='' alt='' /> */}
+      </div>
+      <div className='bg-blue-500'>
+        <h3>h</h3>
+        <p>h</p>
+      </div>
+      <div className='bg-green-500'>
+        <h3>h</h3>
+        <p>h</p>
+      </div>
     </section>
   )
 }
